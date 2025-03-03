851d7e4f2719d187f4f7d5806f7a28b9c459fb34
e.karimi <abnoseek@gmail.com>
2025-03-03 16:46:39 +0330

    feat: trying to handle search

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,17 @@
 import {
   Box,
   Button,
+  Checkbox,
   CircularProgress,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
+  FormControlLabel,
   IconButton,
   Switch,
+  TextField,
   Tooltip,
   Typography,
 } from "@mui/material";
@@ -22,13 +25,43 @@ import { useNavigate } from "react-router-dom";
 import { AppDispatch, RootState } from "../../redux/store";
 import { useEffect, useState } from "react";
 let id: number;
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+
 const Home = () => {
+  const defaultValues = {
+    title: "",
+    description: "",
+    isCompleted: true,
+  };
+  const validationSchema = yup.object({
+    title: yup.string().required("This Field Is Required"),
+    description: yup.string().required("This Field Is Required"),
+    isCompleted: yup.boolean(),
+  });
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    watch,
+    setValue,
+    reset,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues,
+  });
+
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const { tasks, status, error } = useSelector(
     (state: RootState) => state?.task
   );
-  console.log(error, status);
+
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
@@ -52,8 +85,100 @@ const Home = () => {
     navigate(`/task/${taskId}`);
   };
 
+  const onSubmit = () => {};
+
   return (
     <>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
+        <Box className="w-full h-[100vh] flex gap-2 justify-center items-center ">
+          <div className="w-[90%] p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <Box className="grid grid-cols-1 gap-2 mb-3 mt-[10px]">
+              <Controller
+                name="title"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    id="title"
+                    // disabled={doAdd.isLoading || doEdit.isLoading}
+                    label={
+                      <div className="flex gap-1">
+                        <p style={{ fontSize: "14px", color: "red" }}>*</p>
+                        <p>title</p>
+                      </div>
+                    }
+                    inputProps={{
+                      style: {
+                        direction: "ltr",
+                      },
+                    }}
+                    variant="outlined"
+                    placeholder="title"
+                    value={field.value}
+                    onChange={(e) => {
+                      field.onChange(e.target.value);
+                    }}
+                    error={!!errors.title}
+                    helperText={errors.title?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="description"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    id="description"
+                    // disabled={doAdd.isLoading || doEdit.isLoading}
+                    label={
+                      <div className="flex gap-1">
+                        <p style={{ fontSize: "14px", color: "red" }}>*</p>
+                        <p>description</p>
+                      </div>
+                    }
+                    inputProps={{
+                      style: {
+                        direction: "ltr",
+                      },
+                    }}
+                    maxRows={4}
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    placeholder="description"
+                    value={field.value}
+                    onChange={(e) => {
+                      field.onChange(e.target.value);
+                    }}
+                    error={!!errors.description}
+                    helperText={errors.description?.message}
+                  />
+                )}
+              />
+              <FormControlLabel
+                control={
+                  <Controller
+                    name="isCompleted"
+                    control={control}
+                    render={({ field }) => (
+                      <Checkbox
+                        {...field}
+                        checked={!!field.value}
+                        onChange={(e) => field.onChange(e.target.checked)}
+                      />
+                    )}
+                  />
+                }
+                label="completed"
+              />
+              <Box className="flex justify-end items-center">
+                <Button type="submit" color="success" variant="outlined">
+                  search
+                </Button>
+              </Box>
+            </Box>
+          </div>
+        </Box>
+      </form> */}
       <div className=" absolute left-1 top-1">
         <Tooltip title="Add Task">
           <Link to="/task" className="text-blue-500">
@@ -131,7 +256,7 @@ const Home = () => {
       )}
       <Dialog
         open={open}
-        onClose={handleClose}
+        onClose={closeModal}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
@@ -141,7 +266,7 @@ const Home = () => {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>close</Button>
+          <Button onClick={closeModal}>close</Button>
           <Button onClick={handleDeleteTask} autoFocus>
             Agree
           </Button>
